a6bb14cdb6cab587755574e5fcfef81e043a2408
Thibaud Ledent <thibaudledent@gmail.com>
2015-03-11 12:21:11 +0100

    link User to mongo db: REST support

--- src/main/java/org/starfishrespect/myconsumption/server/controllers/UserController.java
+++ src/main/java/org/starfishrespect/myconsumption/server/controllers/UserController.java
@@ -1,18 +1,34 @@
 package org.starfishrespect.myconsumption.server.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.starfishrespect.myconsumption.server.entities.User;
+import org.starfishrespect.myconsumption.server.repositories.UserRepository;
 
 /**
  * Created by thibaud on 11.03.15.
  */
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/user")
-    public User getUserName(@RequestParam(value = "name") String name) {
-        return new User(name);
+    @Autowired
+    private UserRepository repository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public User get(@RequestParam(value = "name") String name) {
+        return repository.findByName(name);
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public User put(@RequestParam(value = "name") String name) {
+        if (repository.findByName(name) != null)
+            return null; // user already exists
+
+        return repository.save(new User(name));
+    }
+
 }
