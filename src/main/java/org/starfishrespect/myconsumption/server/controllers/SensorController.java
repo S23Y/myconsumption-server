fe725692d67ae242af72252253e69539c1a4b6fa
Thibaud Ledent <thibaudledent@gmail.com>
2015-03-11 17:17:53 +0100

    link Sensor to mongo db: adding files

---
+++ src/main/java/org/starfishrespect/myconsumption/server/controllers/SensorController.java
@@ -0,0 +1,184 @@
+package org.starfishrespect.myconsumption.server.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.starfishrespect.myconsumption.server.entities.Sensor;
+import org.starfishrespect.myconsumption.server.repositories.SensorRepository;
+
+import java.util.List;
+
+/**
+ * Created by thibaud on 11.03.15.
+ */
+@RestController
+@RequestMapping("/sensors")
+public class SensorController {
+
+    @Autowired
+    private SensorRepository repository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Sensor> getAllSensors() {
+        return repository.findAll();
+    }
+
+/*    @GET
+    @Path("{sensor}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Sensor get(@PathParam("sensor") String sensorId);
+
+    *//**
+     * Returns the values from a given sensor
+     *
+     * @param sensor
+     * @param startTime
+     * @param endTime
+     * @return
+     *//*
+    @GET
+    @Path("{sensor}/data")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<List<Integer>> valuesForSensor(@PathParam("sensor") String sensor,
+                                               @QueryParam("start") @DefaultValue("0") int startTime,
+                                               @QueryParam("end") @DefaultValue("0") int endTime);
+
+
+    @POST
+    @Path("{sensor}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public SimpleResponseDTO editSensor(@PathParam("sensor") String sensorId,
+                                        @FormParam("name") String name,
+                                        @FormParam("settings") @DefaultValue("") String settings);
+
+    @POST
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public SimpleResponseDTO addSensor(@FormParam("type") @DefaultValue("") String sensorType,
+                                       @FormParam("settings") @DefaultValue("") String settings,
+                                       @FormParam("name") @DefaultValue("") String name,
+                                       @FormParam("user") @DefaultValue("") String linkToUser);
+
+    @DELETE
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public SimpleResponseDTO clear();
+
+    @DELETE
+    @Path("{sensor}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public SimpleResponseDTO removeSensor(@PathParam("sensor") String sensorId);
+    
+    */
+
+
+
+
+ /*    @Override
+    public Sensor get(String sensorId) {
+        try {
+            return dozerBeanMapper.map(sensorController.get(sensorId), Sensor.class);
+        } catch (DaoException e) {
+            // only SENSOR_NOT_FOUND is possible for the moment
+            switch (e.getExceptionType()) {
+                case SENSOR_NOT_FOUND:
+                    throw new NotFoundException();
+                default:
+                    throw new BadRequestException();
+            }
+        }
+    }
+
+    @Override
+    public List<List<Integer>> valuesForSensor(String sensor, int startTime, int endTime) {
+        if (endTime == 0)
+            endTime = Integer.MAX_VALUE;
+        if (startTime < 0 || endTime < 0 || startTime > endTime) {
+            throw new BadRequestException();
+        }
+
+        try {
+            return sensorController.getValues(sensor, startTime, endTime);
+        } catch (DaoException e) {
+            // only SENSOR_NOT_FOUND is possible for the moment
+            switch (e.getExceptionType()) {
+                case SENSOR_NOT_FOUND:
+                    throw new NotFoundException();
+                default:
+                    throw new BadRequestException();
+            }
+        }
+    }
+
+    @Override
+    public SimpleResponse editSensor(String sensorId, String name, String settings) {
+        try {
+            sensorController.editSensor(sensorId, name, settings);
+            return new SimpleResponse(true, "Sensor edited");
+        } catch (DaoException e) {
+            switch (e.getExceptionType()) {
+                case SENSOR_NOT_FOUND:
+                    throw new NotFoundException();
+                case NOTHING_TO_EDIT:
+                    return new SimpleResponse(false, "Nothing changed");
+                case UNKNOWN_SENSOR_TYPE:
+                case CANNOT_CHANGE_SENSOR_TYPE:
+                case BAD_FORMAT_SENSOR_SETTINGS:
+                    throw new BadRequestException();
+                default:
+                    throw new BadRequestException();
+            }
+        }
+    }
+
+    @Override
+    public SimpleResponse addSensor(String sensorType, String settings, String name, String linkToUser) {
+        if (sensorType.equals("") || settings.equals("") || name.equals("")) {
+            throw new BadRequestException();
+        }
+        try {
+            Sensor sensor = sensorController.addSensor(sensorType, settings, name, linkToUser);
+            return new SimpleResponse(true, sensor.getId());
+        } catch (DaoException e) {
+            switch (e.getExceptionType()) {
+                case INVALID_SENSOR_SETTINGS:
+                case BAD_FORMAT_SENSOR_SETTINGS:
+                case UNKNOWN_SENSOR_TYPE:
+                    throw new BadRequestException();
+                case USER_NOT_FOUND:
+                    throw new NotFoundException();
+                default:
+                    throw new BadRequestException();
+            }
+        }
+    }
+
+    @Override
+    public SimpleResponse removeSensor(String sensorId) {
+        try {
+            sensorController.removeSensor(sensorId);
+        } catch (DaoException e) {
+            switch (e.getExceptionType()) {
+                case SENSOR_NOT_FOUND:
+                    throw new NotFoundException();
+                case DATABASE_OPERATION_ERROR:
+                    return new SimpleResponse(false, "error while deleting sensor");
+                default:
+                    throw new BadRequestException();
+            }
+        }
+        return new SimpleResponse(true, "sensor deleted");
+    }
+
+    @Override
+    public SimpleResponse clear() {
+        *//*
+        for (Sensor s:sensorDao.getAllSensors()) {
+            sensorDao.deleteSensor(s.getId());
+        }
+        return new SimpleResponse(true, "database cleared");*//*
+        return new SimpleResponse(false, "function disabled");
+    }*/
+}
---
+++ src/main/java/org/starfishrespect/myconsumption/server/entities/Sensor.java
@@ -0,0 +1,66 @@
+package org.starfishrespect.myconsumption.server.entities;
+
+import java.util.Date;
+
+/**
+ * Created by thibaud on 11.03.15.
+ */
+public class Sensor {
+    private String id;
+    private String name;
+    private String type;
+    private Date firstValue = new Date(0);
+    private Date lastValue = new Date(0);
+    private boolean dead = false;
+
+    public Sensor() { }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getFirstValue() {
+        return firstValue;
+    }
+
+    public void setFirstValue(Date firstValue) {
+        this.firstValue = firstValue;
+    }
+
+    public Date getLastValue() {
+        return lastValue;
+    }
+
+    public void setLastValue(Date lastValue) {
+        this.lastValue = lastValue;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+}
