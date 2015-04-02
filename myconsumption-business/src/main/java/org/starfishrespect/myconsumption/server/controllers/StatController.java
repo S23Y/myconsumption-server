885b05629bb10a3bad6262d90a754555cc82d4cc
Thibaud Ledent <thibaudledent@gmail.com>
2015-04-02 19:42:23 +0200

    config controller added: day and night prices, CO2 equivalent

--- myconsumption-business/src/main/java/org/starfishrespect/myconsumption/server/controllers/StatController.java
+++ myconsumption-business/src/main/java/org/starfishrespect/myconsumption/server/controllers/StatController.java
@@ -1,18 +1,10 @@
 package org.starfishrespect.myconsumption.server.controllers;
 
-/**
- * Created by thibaud on 30.03.15.
- */
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.starfishrespect.myconsumption.server.api.dto.*;
 import org.starfishrespect.myconsumption.server.entities.Stat;
-import org.starfishrespect.myconsumption.server.exceptions.DaoException;
 import org.starfishrespect.myconsumption.server.repositories.StatRepository;
 
-import javax.ws.rs.BadRequestException;
-import javax.ws.rs.NotFoundException;
 import java.util.*;
 
 
