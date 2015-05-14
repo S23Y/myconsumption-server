24402a75d42cd9f0ae5ee30cb9a53268bee54c7d
Thibaud Ledent <thibaudledent@gmail.com>
2015-05-14 10:56:19 +0200

    can access the resource, seems to be connected as thib but i did not provide any credentials which is weird

--- myconsumption-business/src/main/java/org/starfishrespect/myconsumption/server/WebSecurityConfiguration.java
+++ myconsumption-business/src/main/java/org/starfishrespect/myconsumption/server/WebSecurityConfiguration.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.starfishrespect.myconsumption.server.repositories.UserRepository;
+
+import java.util.List;
 
 /**
  * Created by thibaud on 13.05.15.
@@ -35,8 +38,9 @@ public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdap
                 org.starfishrespect.myconsumption.server.entities.User account = mUserRepository.getUser(username);
 
                 if(account != null) {
-                    return new User(account.getName(), account.getPassword(), true, true, true, true,
-                            AuthorityUtils.createAuthorityList("USER"));
+                    List<GrantedAuthority> auth = AuthorityUtils
+                            .commaSeparatedStringToAuthorityList("ROLE_USER");
+                    return new User(account.getName(), account.getPassword(), auth);
                 } else {
                     throw new UsernameNotFoundException("could not find the user '"
                             + username + "'");
