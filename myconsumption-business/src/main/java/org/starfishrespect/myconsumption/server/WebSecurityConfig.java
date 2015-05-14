17fbe6d6191e199dc5f27a678de403f84575d42e
Thibaud Ledent <thibaudledent@gmail.com>
2015-05-14 15:21:48 +0200

    cleaning

--- myconsumption-business/src/main/java/org/starfishrespect/myconsumption/server/WebSecurityConfig.java
+++ myconsumption-business/src/main/java/org/starfishrespect/myconsumption/server/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * Created by thibaud on 13.05.15.
@@ -38,8 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/**")
                 .authenticated()
-                .and().httpBasic();
-                //.and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .and().httpBasic()
+                .and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //
 //        http.authorizeRequests().antMatchers("/sensors").authenticated();
 //        http.csrf().disable().
