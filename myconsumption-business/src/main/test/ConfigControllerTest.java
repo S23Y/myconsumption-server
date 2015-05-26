90f69f7009676ed55f74b480bbcc05118685c95b
Thibaud Ledent <thibaudledent@gmail.com>
2015-05-26 11:14:18 +0200

    working: Config and User controller tests

---
+++ myconsumption-business/src/main/test/ConfigControllerTest.java
@@ -0,0 +1,55 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.hamcrest.Matchers.*;
+
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.context.WebApplicationContext;
+import org.starfishrespect.myconsumption.server.Application;
+import org.starfishrespect.myconsumption.server.controllers.ConfigController;
+
+/**
+ * Created by thibaud on 25.05.15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = Application.class)
+public class ConfigControllerTest {
+
+    @Autowired
+    WebApplicationContext context;
+
+    @InjectMocks
+    ConfigController controller;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void canAccessConfigNoAuth() throws Exception {
+        // @formatter:off
+        mvc.perform(get("/configs/co2")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful());
+        // @formatter:on
+    }
+}
---
+++ myconsumption-business/src/main/test/UserControllerTest.java
@@ -0,0 +1,68 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.context.WebApplicationContext;
+import org.starfishrespect.myconsumption.server.Application;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.starfishrespect.myconsumption.server.controllers.UserController;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by thibaud on 25.05.15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = Application.class)
+public class UserControllerTest {
+
+    @Autowired
+    WebApplicationContext context;
+
+    @InjectMocks
+    UserController controller;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void canAccessUserWithAUth() throws Exception {
+        User user = new User("thib", "hgeO/utTQtK0GiD8asvvas1XSzZlSA3gfxKSn96kPY8=", AuthorityUtils.createAuthorityList("ROLE_USER"));
+        TestingAuthenticationToken testingAuthenticationToken = new TestingAuthenticationToken(user, null);
+        SecurityContextHolder.getContext().setAuthentication(testingAuthenticationToken);
+
+        // @formatter:off
+        MvcResult result = mvc.perform(get("/users/thib")
+                .principal(testingAuthenticationToken))
+                .andExpect(status().isOk())
+                .andReturn();
+        // @formatter:on
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+}
